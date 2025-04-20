362de793e153b9b324cac361b4fb8b0c494d6852
unimadatingapp <unimadatingapp@gmail.com>
2025-04-20 21:26:35 +0200

    nodemailer

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -11,7 +11,7 @@ export class EmailService {
     this.transporter = nodemailer.createTransport({
       service: 'gmail', // Example with Gmail (change according to your provider)
       auth: {
-        user: 'unimadatingapp@gmail.com', // Your email address
+        user: 'bsc-inf-04-22@unima.ac.mw', // Your email address
         pass: 'nfsu vsox qlik hdcc', // Your email password or app password
       },
     });
