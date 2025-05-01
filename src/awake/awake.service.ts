225b6229ce1d8d388e948bd46fd0d502485ea3ee
unimadatingapp <unimadatingapp@gmail.com>
2025-05-01 22:24:01 +0200

    hey

--- src/awake/awake.service.ts
+++ src/awake/awake.service.ts
@@ -9,7 +9,7 @@ export class AwakeService {
 
   constructor(private readonly httpService: HttpService) {}
 
-  @Interval(600000) 
+  @Interval(300000) 
   async callAnotherBackend() {
     try {
       const response$ = this.httpService.get('https://wonge-backend.onrender.com/awake');
